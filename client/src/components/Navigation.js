305318fad60d47d09ee1b9ebc757081d22b84803
steelworx <eileencuevas62@gmail.com>
2019-01-25 16:49:30 -0500

    added styling for navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,12 +6,29 @@ const NavDiv = styled.div`
     width: 100%;
     display: flex;
     justify-content: space-between;
+    background-color: black;
+    color: white;
 
     h1 {
         margin: 1rem;
         font-size: 3rem;
         font-weight: bold;
     }
+
+    p {
+        cursor: pointer;
+    }
+`
+
+const Logo = styled(NavLink)`
+    margin: 1rem;
+    font-size: 3rem;
+    font-weight: bold;
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+    color: white;
+    font-family: 'ZCOOL KuaiLe', cursive;
 `
 
 const LinkContainer = styled.nav`
@@ -23,15 +40,21 @@ const LinkContainer = styled.nav`
 `
 
 const Link = styled(NavLink)`
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+    color: white;
 `
 
-const Navigation = () => {
+const Navigation = props => {
     return(
         <NavDiv>
-            <h1>Dad Jokes</h1>
+            <Logo exact to ='/'>Dad Jokes</Logo>
             <LinkContainer>
                 <Link exact to='/register'>Register</Link>
-                <Link exact to='/login'>Login</Link>
+                {props.jokes.length > 0 ? <p onClick={props.logOut}>Log Out</p> :
+                    <Link exact to='/login'>Log In</Link>
+                }
             </LinkContainer>
         </NavDiv>
     );
